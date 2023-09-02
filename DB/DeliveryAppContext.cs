7733e75e182c17050104206eb3bcc2933704d93a
Fran Perez <franpersanchez@gmail.com>
2023-09-03 00:28:52 +0200

    add default connection params in program.cs

--- DB/DeliveryAppContext.cs
+++ DB/DeliveryAppContext.cs
@@ -3,18 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DB
 {
-    public class AppContext : DbContext
+    public class DeliveryAppContext : DbContext
     {
-        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
-        
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Pedido> Pedidos { get; set; }
-        public DbSet<Vehiculo> Vehiculos { get; set;}
+        public DbSet<Vehiculo> Vehiculos { get; set; }
         public DbSet<HistorialUbicacion> historialUbicaciones { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        public DeliveryAppContext(DbContextOptions<DeliveryAppContext> options) : base(options)
         {
-            base.OnModelCreating(modelBuilder);
         }
 
 
--- Delivery-App-Code-Challenge/Program.cs
+++ Delivery-App-Code-Challenge/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using DB;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 
@@ -6,6 +11,16 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DeliveryAppContext>(
+                options =>
+                {
+                    options.UseNpgsql(conn);
+                    //}, ServiceLifetime.Scoped); // This doesn't work.
+                });
+
 
 var app = builder.Build();
 
