f8b08a73b7398187e39457274a74ba11e0210afe
Fran Perez <franpersanchez@gmail.com>
2023-09-03 00:28:55 +0200

    fix language in SeguimientoPedidoController

--- Delivery-App-Code-Challenge/Controllers/SeguimientoPedidosController.cs
+++ Delivery-App-Code-Challenge/Controllers/SeguimientoPedidosController.cs
@@ -24,25 +24,25 @@ namespace Delivery_App_Code_Challenge.Controllers
 ;
         }
 
-        [HttpGet("/where-is-my-order/{order_id}")]
-        public async Task<ActionResult<IEnumerable<RegistroUbicacion>>> WhereIsMyOrder(long order_id)
+        [HttpGet("/seguimiento-pedido/{id}")]
+        public async Task<ActionResult<IEnumerable<RegistroUbicacion>>> WhereIsMyOrder(long id)
         {
             try
             {
                 // Buscar el pedido por su ID
-                var pedido = await _pedidoRepository.GetSingleOrDefaultAsync(p => p.Id == order_id);
+                var pedido = await _pedidoRepository.GetSingleOrDefaultAsync(p => p.Id == id);
 
                 if (pedido == null)
                 {
-                    return NotFound("Pedido not found");
+                    return NotFound("Pedido no encontrado");
                 }
 
                 // Encontrar uno de los envíos asociados a este pedido, si existe
-                var envio = await _envioRepository.GetSingleOrDefaultAsync(x => x.Pedidos.Any(x => x.Id == order_id));
+                var envio = await _envioRepository.GetSingleOrDefaultAsync(x => x.Pedidos.Any(x => x.Id == id));
 
                 if (envio == null)
                 {
-                    return NotFound("No info for this Envio");
+                    return NotFound("No hay envio asociado a este pedido aún");
                 }
 
                 // Obtener el vehículo asociado al envío
@@ -50,7 +50,7 @@ namespace Delivery_App_Code_Challenge.Controllers
 
                 if (vehiculo == null)
                 {
-                    return NotFound("No info for this Vehiculo");
+                    return NotFound("No hay vehiculo asociado a este pedido aún");
                 }
 
                 // Obtener la lista de registros de ubicación del vehículo ordenada por fecha de registro
