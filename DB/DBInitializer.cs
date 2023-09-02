ecde450646fa90addd5ade22787aef8163fbb10a
Fran Perez <franpersanchez@gmail.com>
2023-09-03 00:28:53 +0200

    fix problem with dependency injection of DbContextFactory

---
+++ DB/DBInitializer.cs
@@ -0,0 +1,42 @@
+﻿using DB.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class DbInitializer
+    {
+        /// <summary>
+        /// Initializes the specified service provider.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
+        public static async void Initialize(IServiceProvider serviceProvider)
+        {
+            //DI
+            var dbContextFactory = serviceProvider.GetRequiredService<IDbContextFactory<DeliveryAppContext>>();
+
+            //Migrations
+            var isConnected = false;
+            using var applicationDbContext = await dbContextFactory.CreateDbContextAsync();
+
+            while (isConnected == false)
+            {
+                try
+                {
+                    applicationDbContext.Database.Migrate();
+                    isConnected = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: " + ex.ToString());
+                }
+                Thread.Sleep(1_000);
+            }
+        }
+    }
+}
--- Delivery-App-Code-Challenge/Program.cs
+++ Delivery-App-Code-Challenge/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using DB;
 using DB.Interfaces;
 using DB.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Accelergreat.EntityFramework;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Data.Entity.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,13 +22,14 @@ builder.Services.AddSwaggerGen();
 var conn = builder.Configuration.GetConnectionString("DefaultConnection");
 Console.WriteLine("Using Connection String: " + conn);
 
-builder.Services.AddDbContext<DeliveryAppContext>(
+builder.Services.AddDbContextFactory<DeliveryAppContext>(
                 options =>
                 {
                     options.UseNpgsql(conn);
                 });
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
 
 var app = builder.Build();
 
