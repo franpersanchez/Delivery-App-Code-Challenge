c49bcbb479d33314a22c56db09737d713ac9a39f
Fran Perez <franpersanchez@gmail.com>
2023-09-03 00:28:52 +0200

    add new PedidosController

---
+++ Delivery-App-Code-Challenge/Controllers/PedidosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Delivery_App_Code_Challenge.Controllers
+{
+    [ApiController]
+    [Route("[controller")]
+    public class PedidosController : ControllerBase
+    {
+        private readonly ILogger<PedidosController> logger;
+
+        public PedidosController(ILogger<PedidosController> logger)
+        {
+            this.logger = logger;
+        }
+    }
+}
